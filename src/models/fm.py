225da003439f9c92177d055aa843155d17fe0854
rejasupotaro <takiguchi0817@gmail.com>
2020-08-10 21:33:03 +0100

    Add FwFM

--- src/models/fm.py
+++ src/models/fm.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 from tensorflow.keras import layers
 
 from src.layers.bias import AddBias0
+from src.layers.interaction import WeightedInteraction
 from src.models.base_model import BaseModel
 
 
@@ -119,4 +120,46 @@ class FMAll(BaseModel):
         biases = AddBias0()(biases)
 
         output = layers.Activation('sigmoid', name='label')(biases + interactions)
+        return tf.keras.Model(inputs=inputs, outputs=output, name=self.name)
+
+
+class FwFM(BaseModel):
+    @property
+    def name(self) -> str:
+        return 'fwfm'
+
+    def build(self):
+        categorical_input_size = {
+            'country': self.total_countries,
+        }
+
+        text_inputs = [
+            self.new_query_input(),
+            self.new_title_input(),
+            self.new_ingredients_input(),
+            self.new_description_input(),
+        ]
+        categorical_inputs = [
+            self.new_country_input(),
+        ]
+
+        inputs = text_inputs + categorical_inputs
+
+        embedding = layers.Embedding(self.total_words, self.embedding_dim, name='text_embedding')
+        text_features = [embedding(text_input) for text_input in text_inputs]
+        text_features = [layers.GlobalMaxPooling1D()(feature) for feature in text_features]
+
+        categorical_features = []
+        for name, categorical_input in zip(categorical_input_size, categorical_inputs):
+            embedding = layers.Embedding(categorical_input_size[name], self.embedding_dim)
+            feature = embedding(categorical_input)
+            feature = tf.reshape(feature, shape=(-1, self.embedding_dim,))
+            categorical_features.append(feature)
+
+        features = text_features + categorical_features
+
+        num_fields = len(features)
+        features = tf.concat(features, axis=1)
+        x = WeightedInteraction(num_fields, name='field_weights')(features)
+        output = layers.Activation('sigmoid', name='label')(x)
         return tf.keras.Model(inputs=inputs, outputs=output, name=self.name)
