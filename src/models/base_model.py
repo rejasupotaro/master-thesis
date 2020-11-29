f599272e53b2c4b8c209c9318a6e5907dcdbec8d
Kentaro Takiguchi <kentaro-takiguchi@cookpad.com>
2020-11-29 18:29:21 +0900

    Enable to use pretrained embeddings

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -1,7 +1,9 @@
 import abc
 
 import numpy as np
+import pandas as pd
 import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras import layers
 
 from src.data.preprocessors import DataProcessor
@@ -9,6 +11,7 @@ from src.data.preprocessors import DataProcessor
 
 class BaseModel(abc.ABC):
     def __init__(self, data_processor: DataProcessor, embedding_dim: int = 32):
+        self.doc_id_encoder = data_processor.doc_id_encoder
         self.total_words = data_processor.total_words
         self.total_authors = data_processor.total_authors
         self.total_countries = data_processor.total_countries
@@ -18,6 +21,9 @@ class BaseModel(abc.ABC):
     @abc.abstractmethod
     def name(self) -> str:
         raise NotImplementedError('Calling an abstract method.')
+
+    def new_doc_id_input(self):
+        return tf.keras.Input(shape=(1,), name='doc_id')
 
     def new_query_input(self, size=6):
         return tf.keras.Input(shape=(size,), name='query')
@@ -36,6 +42,27 @@ class BaseModel(abc.ABC):
 
     def new_country_input(self, size=1):
         return tf.keras.Input(shape=(size,), name='country')
+
+    def load_pretrained_embedding(self, embedding_filepath: str, embedding_dim: int, name: str) -> layers.Embedding:
+        df = pd.read_pickle(embedding_filepath)
+        df.index = df.index.astype(np.int64)
+        classes = self.doc_id_encoder.classes_
+        doc_ids = [doc_id if doc_id in classes else -1 for doc_id in df.index]
+        doc_ids = self.doc_id_encoder.transform(doc_ids)
+        input_dim = len(classes)
+        output_dim = embedding_dim
+        embedding_matrix = np.zeros((input_dim, output_dim))
+        for doc_id, values in zip(doc_ids, df.values):
+            embedding_matrix[doc_id] = values
+
+        embedding = layers.Embedding(
+            input_dim,
+            output_dim,
+            embeddings_initializer=keras.initializers.Constant(embedding_matrix),
+            trainable=False,
+            name=name
+        )
+        return embedding
 
     def ngram_block(self, n_gram: int, total_words: int):
         def wrapped(inputs):
